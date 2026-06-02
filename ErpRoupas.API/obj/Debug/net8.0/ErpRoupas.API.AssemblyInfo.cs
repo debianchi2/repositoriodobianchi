47b0d8f8118b7dd15540f713d81265b0aef80503
Denis Trevisane Bianchi - TI <denis.bianchi@nsop.intra>
2026-06-02 17:35:35 -0300

    feat: adicionado agrupamento retrátil na tabela de estoque

--- ErpRoupas.API/obj/Debug/net8.0/ErpRoupas.API.AssemblyInfo.cs
+++ ErpRoupas.API/obj/Debug/net8.0/ErpRoupas.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ErpRoupas.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c4c6bbb4ef5494ebb841ad55f378b519a5f95d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ErpRoupas.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ErpRoupas.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
